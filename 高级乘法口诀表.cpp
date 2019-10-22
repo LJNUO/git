49035db2f355f46e4a9b3b26d22f01b14d9b2c45
LJNUO <55970248+LJNUO@users.noreply.github.com>
2019-10-22 17:20:14 +0800

    Add files via upload

---
+++ 高级乘法口诀表.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	
+	int a,b,c;
+	for(a=0;a<=8;a++)
+	switch(a){
+	
+	case 0:printf("   |"); 
+	default :printf("%3d",a+1);
+}printf("\n");
+puts("-------------------------------------");
+	int i,j;
+	for(i=1;i<=9;i++){
+	printf("%d  |",i
+	
+	);
+
+			for(j=1;j<=9;j++)
+				printf("%3d",i*j);
+			putchar('\n');}
+		
+	return 0;
+}
